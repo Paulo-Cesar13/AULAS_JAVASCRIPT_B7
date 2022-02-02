3056e1449021a953f1ffc4d6f3be2e3dfae5b690
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-01 23:01:32 -0300

    AULA VARIAVEL 01
    Aprendi o conceito de variável, e como declara-la de forma correta!!

---
+++ VARIAVEL01/indedex.js
@@ -0,0 +1,11 @@
+var x = 10;
+var y = 15;
+var mensagem = "O resultado é:";
+var total = x + y;
+
+window.alert(mensagem+ " " +total);
+
+var nome = 'Paulo César';
+var sobreNome = 'Pereira da Silva';
+
+console.log(nome+sobreNome);
