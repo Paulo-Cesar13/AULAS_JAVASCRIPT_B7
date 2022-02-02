8fda03905b2a13281410768dee19cf2e496e3173
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-02 00:06:03 -0300

    AULA01 VARIAVEIS
    Aprendi sobre a diferença entre var, let e const, e quando usa-las!!

---
+++ VARIAVEL02/script.js
@@ -0,0 +1,24 @@
+/*var nome = "Paulo";
+
+console.log( window.nome );
+
+document.getElementById("titulo").innerHTML = "Opa! tudo bem ?";
+*/
+
+/*
+var nome = "César";
+
+if(nome == "Paulo") {
+    var idade = "44 anos"
+} else{
+    var idade = "Usuario Não encontrado!!"
+}
+
+console.log(idade);
+*/
+
+let debito = 1000;
+let debito2 = 154;
+let totalDebito = debito + debito2;
+
+document.write(totalDebito);
