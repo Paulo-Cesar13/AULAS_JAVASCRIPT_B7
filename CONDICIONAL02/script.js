103cc39ff13936294205a18082e3869312baffec
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-02 21:05:52 -0300

    AULA 4 CONDICIONAL
    Aprendi mais sobre os comandos if,  else e else if. Aprendi sobre a condicional E, que é representado pelo E comercial ( &&), e a condicional ou que é representado pelas barr ( ||).

---
+++ CONDICIONAL02/script.js
@@ -0,0 +1,27 @@
+var hora = 18;
+
+if ( hora == 12 || hora == 18) {
+    window.alert('Você esta na hora do Rosh!!');
+}
+
+
+
+/*
+var hora = 18;
+
+if ( hora >= 12 && hora < 18) {
+    document.write('Boa tarde!!');
+} else {
+    document.write('Que horas são??');
+}
+
+var hora = 08;
+
+if ( hora < 12) {
+    document.write('Bom dia!!');
+} else if ( hora < 18) {
+    document.write('Boa tarde!!');
+} else if ( hora <= 23) {
+    document.write('Boa noite11');
+}
+*/
