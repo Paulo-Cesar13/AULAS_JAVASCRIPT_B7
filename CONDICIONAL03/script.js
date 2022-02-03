cd6c2f4c8ab453594a227824fa99147f4b27fc21
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-03 20:32:47 -0300

    SWITCH CASE
    Aprendi como fazer o switch case, de forma correta!!

---
+++ CONDICIONAL03/script.js
@@ -0,0 +1,34 @@
+var tipoDeUsuario = prompt("Digite seu cargo");
+
+switch (tipoDeUsuario) {
+    case "Adimin":
+        alert("Seu Zèzinho!!");
+        break;
+    case "Gerente":
+        alert("Paulo César!!");
+        break;
+    case "Colaborador":
+        alert("Maria do Reginaldo!!");
+        break;
+    default:
+        alert("Usuario não encontrado!!");
+}
+
+
+/*
+var tipoDeUsuario = "Colaborador";
+
+switch (tipoDeUsuario) {
+    case "Adimin":
+        document.write("Seu Zèzinho!!");
+        break;
+    case "Gerente":
+        document.write("Paulo César!!");
+        break;
+    case "Colaborador":
+        document.write("Maria do Reginaldo!!");
+        break;
+    default:
+        document.write("Usuario não encontrado!!");
+}
+*/
