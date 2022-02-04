b91c46f03ce00650fe05557cb2877c92e00f4e86
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-04 19:00:35 -0300

    LOOP 03
    Trabalhando com o for

---
+++ LOOP 03/script.js
@@ -0,0 +1,5 @@
+var numerosAleatorios = [2, 3, 5, 8, 13, 21];
+
+for (var contador = 0; conatador < numerosAleatorios.length; contador++) {
+    document.write('${contador} - ${numerosAleatorios[contador]}');
+}
