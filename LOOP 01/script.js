10db21189e6d9a83616891f5bf95e56bd4e4daf0
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-04 10:23:52 -0300

    LOOP AULA 01 - WHILE
    Aprendi fazer um laço de repetição com o while, que enquanto uma condição não for atendida ele não para de executar o comendo!

---
+++ LOOP 01/script.js
@@ -0,0 +1,22 @@
+var contador = 0;
+
+while (contador !== 0) {
+    document.write(contador);
+    contador--;
+}
+
+/*
+var contador = 50;
+
+while (contador > 10) {
+    document.write(contador);
+    contador--;
+}
+
+var contador = 10;
+
+while (contador < 10) {
+    document.write(contador);
+    contador++;
+}
+*/
