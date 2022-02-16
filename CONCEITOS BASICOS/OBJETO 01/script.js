cd1014a442ede01ac8ed7ffcaf1f14996c1de720
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-16 20:07:48 -0300

    OBJETO 01
    Um objeto é uma lista nomeada

---
+++ CONCEITOS BASICOS/OBJETO 01/script.js
@@ -0,0 +1,11 @@
+let carro = {
+    fabrica: 'fiat',
+    nome: 'uno',
+    modelo: 'fex',
+    ano: '2018/2019'
+}
+
+window.alert(carro['fabrica']);
+window.alert(carro['nome']);
+window.alert(carro['modelo']);
+window.alert(carro['ano']);
