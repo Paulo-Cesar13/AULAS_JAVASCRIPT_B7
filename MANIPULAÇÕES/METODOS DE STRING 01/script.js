2aee15b659d6c01d9d4c4cfcc2779496a2534e64
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-18 21:18:32 -0300

    MANIPULANDO STRINGS 01
    Aprendi os métodos indexof e o lenght

---
+++ MANIPULAÇÕES/METODOS DE STRING 01/script.js
@@ -0,0 +1,11 @@
+let nome = "Paulo César Pereira da Silva";
+
+let resultado = '';
+
+if(nome.indexOf('Jordan') > -1) {
+    resultado = "Capoeira Bom!!";
+} else {
+    resultado = "Capoeira não encontrado!!"
+}
+
+window.alert(resultado);
