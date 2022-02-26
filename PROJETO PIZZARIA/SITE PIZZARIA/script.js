acfa4871538560c5aab72493f51fd698362b31a6
Paulo César <paulo_cesarcanal@hotmail.com>
2022-02-26 11:21:10 -0300

    PROJETO PIZZARIA
    Bom encontrei o erro no código, que eu não tinha visto, e dava problema, alterei, o css do projeto, acrescentando uma margem no h1, porque quando abria no mobile, o header estava comendo parte do nome pizza.

--- PROJETO PIZZARIA/SITE PIZZARIA/script.js
+++ PROJETO PIZZARIA/SITE PIZZARIA/script.js
@@ -103,15 +103,29 @@ dq('.pizzaInfo--addButton').addEventListener('click', ()=>{
     closeModal();
 });
 
+dq('.menu-openner').addEventListener('click', ()=>{
+    if(cart.length > 0) {
+        dq('aside').style.left = '0';
+    }
+});
+dq('.menu-closer').addEventListener('click', ()=>{
+    dq('aside').style.left = '100vw';
+});
+
 function updateCart() {
+    dq('.menu-openner span').innerHTML = cart.length;
     if(cart.length > 0) {
         dq('aside').classList.add('show');
         dq('.cart').innerHTML ='';
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
             let cartItem = dq('.models .cart--item').cloneNode(true);
             let pizzaSizeName;
-            switch(cart[i].size) {
+            switch (cart[i].size) {
                 case 0:
                     pizzaSizeName = 'P';
                     break;
@@ -121,13 +135,33 @@ function updateCart() {
                 case 2:
                     pizzaSizeName = 'G';
                     break;
-            };
-            let pizzaName = `${pizzaItem.name} ${pizzaSizeName}`;
+            }
+            let pizzaName = `${pizzaItem.name}(${pizzaSizeName})`;
             cartItem.querySelector('img').src = pizzaItem.img;
-            cartItem.querySelector('.cart--item-name').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
+
             dq('.cart').append(cartItem);
         }
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+        dq('.subtotal span:Last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        dq('.desconto span:Last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        dq('.total span:Last-child').innerHTML = `R$ ${total.toFixed(2)}`;
     } else {
         dq('aside').classList.remove('show');
+        dq('aside').style.left = '100vw';
     }
 };
